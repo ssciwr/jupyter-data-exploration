5aa5f18b386929772132989f219b12edde59fb9e
Liam Keegan <liam@keegan.ch>
2024-05-08 16:48:27 +0200

    add header via custom nbconvert template

--- jupyter_nbconvert_config.py
+++ jupyter_nbconvert_config.py
@@ -1,8 +1,8 @@
 c = get_config()  # noqa
 c.ExecutePreprocessor.enabled = True
 c.NbConvertApp.export_format = "slides"
-# c.SlidesExporter.extra_template_paths = []
+c.SlidesExporter.extra_template_paths = ["."]
 c.SlidesExporter.file_extension = ".html"
 c.SlidesExporter.reveal_transition = "convex"
 c.SlidesExporter.reveal_scroll = True
-# c.SlidesExporter.template_file = None
+c.SlidesExporter.template_name = "jupyter_nbconvert_template"
